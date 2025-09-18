923d293fc929a891a33614cf79e94c98452d1e03
Abdus Samad <abdussamadshamsi486@gmail.com>
2025-09-18 23:53:34 +0530

    backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
 // Add JWT token to requests if logged in
 API.interceptors.request.use((req) => {
